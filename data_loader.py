b6da9825a7dbb02b22b628c6678e8fcf9d082f53
MS <maz.zeesal@gmail.com>
2024-10-22 10:41:01 +0900

    Create data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+import pandas as pd
+class CustomDataset(Dataset):
+    def __init__(self, csv_path):
+        self.data = pd.read_csv(csv_path)
+        self.inputs = self.data.iloc[:, :-1].values
+       # print(self.inputs.shape())
+        self.labels = self.data.iloc[:, -1].values
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        input_tensor = torch.tensor(self.inputs[idx], dtype=torch.float32)
+        label_tensor = torch.tensor(self.labels[idx], dtype=torch.long)
+        return input_tensor, label_tensor
+
